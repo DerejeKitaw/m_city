049bbc2d88a7c9fa65fc9b2ff8a43a1d7e45fff1
Dereje Kitaw <dereje.kitaw@gmail.com>
2018-09-07 17:00:22 -0400

    34_Home- Finishing the Match blocks component

--- src/Components/home/index.js
+++ src/Components/home/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Featured from './featured'
 import MatchHome from './matches'
+import MeetPlayers from './meetPlayers'
 
 const Home = () => {
   return (
     <div className="bck_blue">
       <Featured />
       <MatchHome />
+      <MeetPlayers />
     </div>
   )
 }
