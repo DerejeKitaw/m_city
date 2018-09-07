a87ee06a611e714445214263d02478c646095c21
Dereje Kitaw <dereje.kitaw@gmail.com>
2018-09-06 22:21:48 -0400

    28-29-stripe animation

--- src/Routes.js
+++ src/Routes.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import Layout from './Hoc/Layout';
-
+import { Switch, Route } from 'react-router-dom';
+import Home from './Components/home'
 const Routes = (props) => {
   return (
-    <Layout>Seconed Line</Layout>
+    <Layout>
+      <Switch>
+        <Route exact component={Home} path="/" />
+      </Switch>
+    </Layout>
   )
 }
 
